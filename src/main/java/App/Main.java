977d529e9736b22bd53d70c2702363c6b003542a
Julian <juliangarciapetcov16@gmail.com>
2026-04-09 17:58:30 +0200

    Ultimo push

--- src/main/java/App/Main.java
+++ src/main/java/App/Main.java
@@ -21,53 +21,58 @@ public class Main {
          * 1. Filtrar los registros de temperatura que sean mayores a 25 grados, la humedad sea menor a 70 y la
          * fecha sea anterior a la fecha actual, y mostrarlos.
          */
-        IO.println("--------------------------------");
+        IO.println("----- Ejercicio 1 -----");
         lista.stream()
                 .filter(r -> r.getTemperatura() > 25)
                 .filter(r -> r.getHumedad() > 70)
                 .filter(r -> r.getFechaHora().isAfter(LocalDateTime.now()))
                 .forEach(System.out::println);
 
+
         /**
          * 2. Encontrar el registro con la temperatura más alta y mostrar el registro completo.
          */
-        IO.println("------------------------------");
+        IO.println("----- Ejercicio 2 -----");
         lista.stream()
                 .sorted(Comparator.comparing(Registro::getTemperatura).reversed())
                 .limit(1)
                 .forEach(System.out::println);
 
+
         /**
          * 3. Obtener una lista con las fechas/horas de todas las tomas de datos.
          */
-        IO.println("-----------------------------");
+        IO.println("----- Ejercicio 3 -----");
+        lista.stream()
+                .map(Registro::getFechaHora)
+                .forEach(System.out::println);
 
 
         /**
          * 4. Incrementar en 5 unidades la humedad de todos los registros y mostrar las temperaturas,
          * humedades y fechas/horas actualizadas.
          */
-        IO.println("------------------------------");
-        lista.stream()
-                .mapToDouble(Registro::getTemperatura)
-                .forEach(System.out::println);
+        IO.println("----- Ejercicio 4 -----");
+        IO.println(" No se hacerlo :( ");
+
 
         /**
          * 5. Encontrar el registro con la temperatura más baja que tenga una humedad mayor a 80 y mostrar la
          * temperatura, humedad y fecha.
          */
-        IO.println("------------------------------");
+        IO.println("----- Ejercicio 5 -----");
         lista.stream()
                 .filter(r -> r.getHumedad() > 80)
                 .sorted(Comparator.comparing(Registro::getTemperatura))
                 .limit(1)
                 .forEach(System.out::println);
 
+
         /**
          * 6. Verificar si algún registro tiene una temperatura mayor a 30 grados, humedad mayor a 90 y la fecha
          * es de hoy. Mostrar un mensaje indicando si hay algún registro que cumple esta condición o no.
          */
-        IO.println("------------------------------");
+        IO.println("----- Ejercicio 6 -----");
         boolean registro = lista.stream()
                 .anyMatch(r -> r.getTemperatura() > 80
                 && r.getHumedad() > 90
@@ -78,41 +83,42 @@ public class Main {
         /**
          * 7. Muestra 10 registros saltándote los 5 primeros.
          */
-        IO.println("-----------------------------");
+        IO.println("----- Ejercicio 7 -----");
         lista.stream()
                 .skip(5)
                 .limit(10)
                 .forEach(System.out::println);
 
+
         /**
          * 8. Muestra los registros ordenados por fecha (sorted(Comparator))
          */
-        IO.println("------------------------------");
+        IO.println("----- Ejercicio 8 -----");
         lista.stream()
                 .sorted(Comparator.comparing(Registro::getFechaHora).reversed())
                 .forEach(System.out::println);
 
+
         /**
          * 9. Cuenta los registros que tengan temperatura mayor a 35 grados (count()).
          */
-        IO.println("------------------------------");
+        IO.println("----- Ejercicio 9 -----");
         long cuenta = lista.stream()
                 .filter(r -> r.getTemperatura() > 35)
                 .count();
         IO.println("Registros: " + cuenta);
 
+
         /**
          * 10. Calcular la temperatura promedio de todos los registros (transformarlo en Stream<Double> y
          * llamar a average()).
          */
-        IO.println("------------------------------");
-        Double temperatura = lista.stream()
+        IO.println("----- Ejercicio 10 -----");
+        double temperatura = lista.stream()
                 .mapToDouble(Registro::getTemperatura)
                 .average()
                 .orElse(0);
         IO.println("Temperatura media: " + temperatura + " Grados");
-
-
 
 
     }
